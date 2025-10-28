6ab82f5286a54baa2d71c1e42386729efb98c818
syntax-punk <david.jaeren@gmail.com>
2025-10-28 14:58:30 +0100

    Initial

---
+++ src/main/java/com/syntaxpunk/axonddd101/AxonDdd101Application.java
@@ -0,0 +1,13 @@
+package com.syntaxpunk.axonddd101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxonDdd101Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AxonDdd101Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/syntaxpunk/axonddd101/AxonDdd101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syntaxpunk.axonddd101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxonDdd101ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
