6127081055b742d5c7d9930f412456d4e9e036af
syntax-punk <david.jaeren@gmail.com>
2025-11-15 22:55:20 +0100

    Added a Jacksom serialization due to Java records

--- rental/src/main/java/com/syntaxpunk/rental/RentalApplication.java
+++ rental/src/main/java/com/syntaxpunk/rental/RentalApplication.java
@@ -1,10 +1,29 @@
 package com.syntaxpunk.rental;
 
+import com.thoughtworks.xstream.XStream;
+import org.axonframework.serialization.Serializer;
+import org.axonframework.serialization.json.JacksonSerializer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RentalApplication {
+
+    @Bean
+    public Serializer messageSerializer() {
+        return JacksonSerializer.builder()
+                .defaultTyping()
+                .build();
+    }
+
+    @Bean
+    public Serializer eventSerializer() {
+        return JacksonSerializer.builder()
+                .defaultTyping()
+                .build();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(RentalApplication.class, args);
